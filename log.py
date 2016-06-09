29990620c164c7f0ead9789f3b19e6995da91c96
norang <pinethee@naver.com>
2016-06-09 21:50:44 +0000

    콘솔 출력 부분 추가 minor error fix

--- log.py
+++ log.py
@@ -1,9 +1,21 @@
 import logging
 import logging.handlers
+import sys
 
 # log file 만들기
 # '/'로 시작하는 command와 샤샤의 심심이 기능을 분리해서 로그를 남긴다
 
+# 최상위 loger에게 stdout으로 출력하도록 한다
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s|%(name)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+# child logger는 file에 출력한다
 # command log용 hander 생성
 cmd_logger = logging.getLogger('cmdLoger')
 cmd_fileHandler = logging.FileHandler('Logs/command.log') 
@@ -27,8 +39,8 @@ cmd_fileHandler.setFormatter(formatter)
 chat_logger.addHandler(chat_fileHandler)
 cmd_logger.addHandler(cmd_fileHandler)
 
-chat_logger.addHandler(streamHandler)
-cmd_logger.addHandler(fileHandler)
+chat_logger.addHandler(chat_streamHandler)
+chat_logger.addHandler(cmd_streamHandler)
 
 cmd_logger.setLevel(logging.DEBUG)
 chat_logger.setLevel(logging.DEBUG)
