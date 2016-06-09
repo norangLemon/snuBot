f91d4ad685da2f2575700f994da3ea77ad872966
norang <pinethee@naver.com>
2016-06-09 21:22:14 +0000

    로그 추가

---
+++ log.py
@@ -0,0 +1,41 @@
+import logging
+import logging.handlers
+
+# log file 만들기
+# '/'로 시작하는 command와 샤샤의 심심이 기능을 분리해서 로그를 남긴다
+
+# command log용 hander 생성
+cmd_logger = logging.getLogger('cmdLoger')
+cmd_fileHandler = logging.FileHandler('Logs/command.log') 
+cmd_streamHandler = logging.StreamHandler()
+
+# 심심이 log용 handler 생성
+chat_logger = logging.getLogger('chatLoger')
+chat_fileHandler = logging.FileHandler('Logs/chat.log') 
+chat_streamHandler = logging.StreamHandler()
+
+# 동등한 파일 형식 사용
+
+#     [파일명: 줄번호] <레벨>
+# 메시지
+formatter = logging.Formatter('\t[%(filename)s: %(lineno)s] <%(levelname)s> %(asctime)s\n%(message)s')
+
+# 형식 적용, 핸들러를 로거에 추가, 레벨 설정
+chat_fileHandler.setFormatter(formatter)
+cmd_fileHandler.setFormatter(formatter)
+
+chat_logger.addHandler(chat_fileHandler)
+cmd_logger.addHandler(cmd_fileHandler)
+
+chat_logger.addHandler(streamHandler)
+cmd_logger.addHandler(fileHandler)
+
+cmd_logger.setLevel(logging.DEBUG)
+chat_logger.setLevel(logging.DEBUG)
+
+# 함수명 alias
+cmd_prtErr = cmd_logger.error
+cmd_prtLog = cmd_logger.debug
+
+chat_prtErr = chat_logger.error
+chat_prtLog = chat_logger.debug
